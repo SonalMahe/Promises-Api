173082f1bb7f54ee187f98e8c345976d4418b3bd
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-02-05 12:44:47 +0100

    html pages codes
    -used element by id and eventlistener for clicking the button n get results.
    -used inline styling for button
    -

--- promises.js
+++ promises.js
@@ -6,41 +6,53 @@ const checkStudyEnergy = () => {
         let energyLevel = Math.random();
 
         if (energyLevel > 0.5) {
-            resolve("Energy level is high:) ! Ready to study.");
+            resolve("Energy level is high! Ready to study.");
         } else {
-            reject("Energy level is low :( .Take a break first.")
+            reject("Energy level is low. Take a break first.")
         }
     });
 }
 checkStudyEnergy()
     .then((message) => {
-            console.log(message);
-        })
+        console.log(message);
+    })
 
-   .catch((error) => {
-    console.log("Error:" + error);
-});
+    .catch((error) => {
+        console.log("Error:" + error);
+    });
 
 
 
 // part 2.Fetching Data from an API- (using country details API)-
+function fetchCountryData() {
+    return fetch("https://restcountries.com/v3.1/name/norway") // use Norway country for checkimg on webpage
+        .then(response => response.json())
+        .then(data => {
+            return `
+                  🌍 Country: ${data[0].name.official}
+                  🏛 Capital: ${data[0].capital[0]}
+                  🌐 Region: ${data[0].region}
+            `;
+        })
+        .catch(error => {
+            return "Error fetching country data";
+        });
+}
 
-const fetchCountryData = (countryname) => {
-const promise = fetch(`https://restcountries.com/v3.1/name/${countryname}`) 
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(" 🌍 Country :-" , data[0].name.official)
-        console.log(" 🏛️ Capital :-", data[0].capital[0])
-        console.log(" 🌐 Region :-", data[0].region)
+// fetchCountryData("sweden");
+// //  fetchCountryData("italy");
+// //  fetchCountryData("france");
+
+
+//3 .Mix it up both parts-
+
+checkStudyEnergy()
+    .then((message) => {
+        console.log(message);
+        return fetchCountryData(); // Fetch country data 
     })
     .catch((error) => {
-        console.log('error fetching data:', error);
-    })
-};
-
-fetchCountryData("sweden");
-fetchCountryData("italy");
-fetchCountryData("france");
-
+        console.log("Error fetching data", error);
+    });
 
 
